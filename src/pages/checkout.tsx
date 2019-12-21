8589d3b75560895d37024fc8b36e9e4550478597
exshak <sbijabhai@gmail.com>
2019-12-20 20:26:11 -0500

    integrated stripe checkout, redux-logger development only, updated favicon & title

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import CheckoutItem from '../components/checkoutItem'
+import StripeButton from '../components/common/button/stripe'
 import { selectCartItems, selectCartTotal } from '../redux/cart/cartSelectors'
 import './styles.scss'
 
@@ -28,6 +29,12 @@ const CheckoutPage = ({ cartItems, total }: any) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className='total'>TOTAL: ${total}</div>
+    <div className='test-warning'>
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+    <StripeButton price={total} />
   </div>
 )
 
